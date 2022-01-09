9bfb3822f485ed774f61ba1a52a10f41b8704015
DoNotHaveAnIdeaForAUsername <louie.decierdo@gmail.com>
2022-01-09 18:33:42 +0800

    Added a catch statement in the get request

--- src/resolvers/PostResolver.ts
+++ src/resolvers/PostResolver.ts
@@ -1,17 +1,26 @@
 import { Query, Resolver } from 'type-graphql';
+import { ApolloError } from 'apollo-server-errors';
 import { PostType } from '../types/PostType';
 import axios from 'axios';
 
 @Resolver()
 export class PostResolver {
-  @Query(() => [PostType], { nullable: true })
-  async posts() {
-    var data = null;
+  @Query(() => [PostType])
+  async users() {
+    var data: PostType[] | null = null;
+    var error: string | null = null;
     await axios
       .get('https://jsonplaceholder.typicode.com/posts')
       .then((res) => {
         data = res.data;
+      })
+      .catch((err) => {
+        error = err.toString();
       });
+
+    if (error) {
+      throw new ApolloError('An error occured', error);
+    }
 
     return data;
   }
--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,17 +1,26 @@
 import { Query, Resolver } from 'type-graphql';
+import { ApolloError } from 'apollo-server-errors';
 import { UserType } from '../types/UserType';
 import axios from 'axios';
 
 @Resolver()
 export class UserResolver {
-  @Query(() => [UserType], { nullable: true })
+  @Query(() => [UserType])
   async users() {
-    var data = null;
+    var data: UserType[] | null = null;
+    var error: string | null = null;
     await axios
       .get('https://jsonplaceholder.typicode.com/users')
       .then((res) => {
         data = res.data;
+      })
+      .catch((err) => {
+        error = err.toString();
       });
+
+    if (error) {
+      throw new ApolloError('An error occured', error);
+    }
 
     return data;
   }
